e5dc37e5093adadcbd9928ee1e6895b222b7b5be
dicenull <dicenull.dev@gmail.com>
2025-10-16 19:47:33 +0900

    100こ点表示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:app/pages/fragment_shader_pod.dart';
 import 'package:app/shader_painter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomePage extends HookConsumerWidget {
@@ -11,39 +10,48 @@ class HomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final shaderValue = ref.watch(fragShaderPodProvider);
-
-    ref.listen(fragShaderPodProvider, (_, next) {
-      next.whenData((shader) {
-        // 100個のランダムな点を生成してシェーダーに渡す
-        final rnd = Random();
-        final points = List.generate(100, (_) {
-          final x = rnd.nextDouble() * 2 - 1;
-          final y = rnd.nextDouble() * 2 - 1;
-          return [x, y];
-        }).expand((e) => e).toList();
-
-        for (var i = 0; i < points.length; i++) {
-          shader.setFloat(2 + i, points[i]);
-        }
-      });
-    });
+    final mousePositions = useState<List<Offset>>([]);
+    final index = useState(0);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Title'),
       ),
-      body: CustomPaint(
-        painter: switch (shaderValue) {
-          AsyncData(:final value) => ShaderPainter(
-              shader: value,
-            ),
-          _ => null,
+      body: MouseRegion(
+        onHover: (event) {
+          // ウィンドウのサイズを取得
+          final size = MediaQuery.of(context).size;
+
+          // マウス座標を取得
+          final x = event.position.dx;
+          final y = event.position.dy;
+
+          // 中央を0として、-1.0~1.0の範囲に正規化
+          final normalizedX = (x - size.width / 2) / (size.width / 2);
+          final normalizedY = (y - size.height / 2) / (size.height / 2);
+
+          final point = Offset(normalizedX, normalizedY);
+          if (mousePositions.value.length < 100) {
+            mousePositions.value.add(point);
+          } else {
+            mousePositions.value[index.value] = point;
+            index.value = (index.value + 1) % 100;
+          }
         },
-        child: Center(
-          child: Container(
-            width: 100,
-            height: 150,
-            color: Colors.amber,
+        child: CustomPaint(
+          foregroundPainter: switch (shaderValue) {
+            AsyncData(:final value) => ShaderPainter(
+                shader: value,
+                points: mousePositions.value,
+              ),
+            _ => null,
+          },
+          child: Center(
+            child: Container(
+              width: 100,
+              height: 150,
+              color: Colors.amber,
+            ),
           ),
         ),
       ),
--- lib/shader_painter.dart
+++ lib/shader_painter.dart
@@ -3,18 +3,28 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ShaderPainter extends CustomPainter {
-  const ShaderPainter({required this.shader});
+  const ShaderPainter({
+    required this.shader,
+    required this.points,
+  });
 
   final FragmentShader shader;
+  final List<Offset> points;
 
   @override
   void paint(Canvas canvas, Size size) {
     shader.setFloat(0, size.width);
     shader.setFloat(1, size.height);
 
+    for (var i = 0; i < points.length; i++) {
+      shader.setFloat(2 + i * 2, points[i].dx); // -1.0 ~ 1.0
+      shader.setFloat(3 + i * 2, points[i].dy); // -1.0 ~ 1.0
+    }
     canvas.drawRect(Offset.zero & size, Paint()..shader = shader);
   }
 
   @override
-  bool shouldRepaint(covariant ShaderPainter oldDelegate) => false;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
 }
