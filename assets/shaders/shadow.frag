4e8dd3a5c8a4066655d9d4c6e11640059ed3504b
dicenull <dicenull.dev@gmail.com>
2025-10-16 19:15:38 +0900

    点の周りを明るくした

--- assets/shaders/shadow.frag
+++ assets/shaders/shadow.frag
@@ -5,11 +5,25 @@
 precision mediump float;
 
 uniform vec2 uSize;
+uniform vec2 points[100];
 out vec4 fragColor;
 
 void main() {
-  vec2 coord = FlutterFragCoord().xy;
-  vec2 uv = (coord  - 0.5 * uSize.xy) / uSize.y;
+    vec2 coord = FlutterFragCoord().xy;
+    vec2 uv = (coord - 0.5 * uSize.xy) / uSize.y;
 
-  fragColor = vec4(vec3(1.0 - length(uv)), 1.0);
+    vec4 color = vec4(vec3(0.2), 1.0);
+
+    float d = 1.0;
+    for (int i = 0; i < 100; i++) {
+        vec2 p = points[i];
+        d = min(d, distance(uv, p));
+    }
+    
+    if (d < 0.1) {
+        fragColor = vec4(color.rgb, smoothstep(0, 1, d / 0.1));
+        return;
+    }
+
+  fragColor = color;
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:app/pages/fragment_shader_pod.dart';
 import 'package:app/shader_painter.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +11,22 @@ class HomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final shaderValue = ref.watch(fragShaderPodProvider);
+
+    ref.listen(fragShaderPodProvider, (_, next) {
+      next.whenData((shader) {
+        // 100個のランダムな点を生成してシェーダーに渡す
+        final rnd = Random();
+        final points = List.generate(100, (_) {
+          final x = rnd.nextDouble() * 2 - 1;
+          final y = rnd.nextDouble() * 2 - 1;
+          return [x, y];
+        }).expand((e) => e).toList();
+
+        for (var i = 0; i < points.length; i++) {
+          shader.setFloat(2 + i, points[i]);
+        }
+      });
+    });
 
     return Scaffold(
       appBar: AppBar(
